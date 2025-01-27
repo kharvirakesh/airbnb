643f9530055bff3443fb670415c96400c4601d05
kharvirakesh <kharvirakesh3@gmail.com>
2025-01-27 17:58:38 +0530

    feat: create profile action

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,14 +1,40 @@
 'use server';
 import { profileSchema } from "./schemas";
+import db from './db';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
-export const createProfileAction = async (prevState: unknown, formData: FormData) => {
-    try {
-        const rawData = Object.fromEntries(formData);
-            const validatedFields = profileSchema.parse(rawData);
-            console.log(validatedFields);
-            return { message: 'Profile Created' };
-    } catch (error) {
-        console.log(error);
-            return { message: 'there was an error...' };
-    }
+export const createProfileAction = async (
+  prevState: unknown,
+  formData: FormData
+) => {
+  try {
+    const user = await currentUser();
+    if (!user) throw new Error('Please login to create a profile');
+
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
+  }
+  redirect('/');
 };
